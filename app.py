b39f5f0dba26464f005891090b51964588894cb2
HARSH BANSAL <146634355+HARSHBANSAL2007@users.noreply.github.com>
2026-07-27 13:35:21 +0530

    Refactor app.py for resume generation functionality

--- app.py
+++ app.py
@@ -78,5 +78,5 @@ if st.button('generate resume'):
     print(response['messages'][-1].content)
     code=response['messages'][-1].content[-1]['text']
     #st.markdown(code)
-    st.html(code , width="streach" , unsafe_allow_javascript=True)
+    st.html(code , width="streTch" , unsafe_allow_javascript=True)
     
