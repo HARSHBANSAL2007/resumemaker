fc5bd75fe8f75e6851e71645d5c7c4cfd12ee728
HARSH BANSAL <146634355+HARSHBANSAL2007@users.noreply.github.com>
2026-07-27 13:32:03 +0530

    Refactor app.py for resume generation functionality

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ import numpy as np
 import streamlit as st
 from langchain_community.document_loaders import PyMuPDFLoader
 # api keys
-GEMINI= st.sidebar.text_input("GEMINI",type="password")
+GOOGLE= st.sidebar.text_input("GEMINI",type="password")
 GROQ= st.sidebar.text_input("GROQ",type="password")
 TAVILY =st.sidebar.text_input("TAVILY",type="password")
 #====================================================
